a0c0b0c5a6e1bc31c81b7bef98cee43e1813581b
Matt Fried <mfried@WOLFROMENG.local>
2019-05-29 13:26:37 -0500

    updated app.js with count variable and isReady function

--- public/app.js
+++ public/app.js
@@ -39,290 +39,292 @@ const showStandings = (e) => {
 
 // Check if all 4 league data requests are complete
 // If so, .btnContainer appears & user prompt text changes
-// const isReady = () => {
-//   if (count === 4) {
-//     document.querySelector('#select').classList.remove('infinite');
-//     document.querySelector('#select').classList.add('bounceIn');
-//     // change user prompt text
-//     document.querySelector('#select').innerHTML = 'Please select a League';
-//     // unhide btnContainer containing league buttons
-//     document.querySelector('#btnContainer').style.opacity = '1';
-//   }
-// }
+let count = 0;
+const isReady = () => {
+  if (count === 4) {
+    document.querySelector('#select').classList.remove('infinite');
+    document.querySelector('#select').classList.add('bounceIn');
+    // change user prompt text
+    document.querySelector('#select').innerHTML = 'Please select a League';
+    // unhide btnContainer containing league buttons
+    document.querySelector('#btnContainer').style.opacity = '1';
+  }
+}
 
 
+// Get standings function to call for each league
+const getStandings = (data, totalTeams, confs, firstDivsInConfs, divisions, divisionStandings, overallStandings, league) => {
 
-  // Get standings function to call for each league
-  const getStandings = (data, totalTeams, confs, firstDivsInConfs, divisions, divisionStandings, overallStandings, league) => {
+  let content = '';
 
-    let content = '';
+  /* if MYSPORTSFEEDS response data misses a rank #,
+  then object property will be undefined in for loops below.
+  console error will be logged as per for loops below...*/
 
-    /* if MYSPORTSFEEDS response data misses a rank #,
-    then object property will be undefined in for loops below.
-    console error will be logged as per for loops below...*/
-
-    // PUSH TEAMS INTO OVERALL STANDINGS, BASED ON OVERALL RANK
-    for (let rank = 1; rank <= totalTeams; rank++ ) {
-      for (let i = 0; i < totalTeams; i++) {
-        if (data.teams[i].overallRank.rank === rank) {
-          overallStandings.push(data.teams[i]);
-        }
+  // PUSH TEAMS INTO OVERALL STANDINGS, BASED ON OVERALL RANK
+  for (let rank = 1; rank <= totalTeams; rank++ ) {
+    for (let i = 0; i < totalTeams; i++) {
+      if (data.teams[i].overallRank.rank === rank) {
+        overallStandings.push(data.teams[i]);
       }
     }
+  }
 
-    console.log(league + ' overall standings:');
-    console.log(overallStandings);
+  console.log(league + ' overall standings:');
+  console.log(overallStandings);
 
-    // ORGANIZE OVERALL STANDINGS INTO DIVISIONS
-    // for each division
-    for (let d = 0; d < divisions.length; d++) {
-      let current = [];
+  // ORGANIZE OVERALL STANDINGS INTO DIVISIONS
+  // for each division
+  for (let d = 0; d < divisions.length; d++) {
+    let current = [];
 
-      // for each team
-      for (let t = 0; t < data.teams.length; t++) {
-        /* NFL response data object from MYSPORTSFEEDS may have skipped a rank #,
-        therefore overallStandings object is missing team(s) */
+    // for each team
+    for (let t = 0; t < data.teams.length; t++) {
+      /* NFL response data object from MYSPORTSFEEDS may have skipped a rank #,
+      therefore overallStandings object is missing team(s) */
 
-        //  console log error message if object property is undefined
-        if (typeof overallStandings[t] === 'undefined') {
-          console.log(`${league} team with index ${t} could not be found in overallStandings object,
-                          when looping through ${divisions[d]} division. Team(s) may be missing from the DOM standings`);
-        }
-        // MLB league response data has duplicate division names,
-        // therefore counter variable is used to distinguish which conference
-        else if (league === 'mlb') {
-          let counter = 0;
-          if (d >= 3) {
-            counter = 1;
-            }
-          if ((overallStandings[t].divisionRank.divisionName === divisions[d]) && (overallStandings[t].conferenceRank.conferenceName === confs[counter])) {
-            current.push(overallStandings[t]);
-          }
-        }
-        // all leagues other than MLB
-        else if (overallStandings[t].divisionRank.divisionName === divisions[d]) {
-          current.push(overallStandings[t]);
-        }
-
-      } // end of each team loop
-
-      divisionStandings.push(current);
-    } // end of each division loop
-
-    console.log(league + ' division standings:');
-    console.log(divisionStandings);
-
-
-    // main title html
-    content += `
-      <div class="title">
-        <h1>${league}: Regular Season Standings
-    `;
-
-    if (league === "nhl") {
-      content += nhlSeason;
-    } else if (league === "nba") {
-      content += nbaSeason;
-    } else if (league === "nfl") {
-      content += nflSeason;
-    } else {
-      content += mlbSeason;
-    }
-
-    content += `
-        </h1>
-      </div>
-    `;
-
-    // each conference html
-    for (let div = 0; div < divisions.length; div++) {
+      //  console log error message if object property is undefined
+      if (typeof overallStandings[t] === 'undefined') {
+        console.log(`${league} team with index ${t} could not be found in overallStandings object,
+                        when looping through ${divisions[d]} division. Team(s) may be missing from the DOM standings`);
+      }
       // MLB league response data has duplicate division names,
-      // therefore divCounter variable is used to distinguish which conference
-      if (league === 'mlb') {
-      let divCounter = 0;
-        if (div >= 3) {
-          divCounter = 1;
-        }
-
-        if ((divisions[div] === firstDivsInConfs[0]) && (divCounter === 0)) {
-          content += `
-            <div class="conf-title">
-              <h2>${confs[0]} Conference</h2>
-            </div>
-          `;
-        } else if ((divisions[div] === firstDivsInConfs[1]) && (divCounter === 1)) {
-            content += `
-              <div class="conf-title">
-                <h2>${confs[1]} Conference</h2>
-              </div>
-            `;
+      // therefore counter variable is used to distinguish which conference
+      else if (league === 'mlb') {
+        let counter = 0;
+        if (d >= 3) {
+          counter = 1;
+          }
+        if ((overallStandings[t].divisionRank.divisionName === divisions[d]) && (overallStandings[t].conferenceRank.conferenceName === confs[counter])) {
+          current.push(overallStandings[t]);
         }
       }
       // all leagues other than MLB
-      else {
-        if (divisions[div] === firstDivsInConfs[0]) {
-          content += `
-            <div class="conf-title">
-              <h2>${confs[0]} Conference</h2>
-            </div>
-          `;
-        } else if (divisions[div] === firstDivsInConfs[1]) {
-            content += `
-              <div class="conf-title">
-                <h2>${confs[1]} Conference</h2>
-              </div>
-            `;
-        }
+      else if (overallStandings[t].divisionRank.divisionName === divisions[d]) {
+        current.push(overallStandings[t]);
       }
 
+    } // end of each team loop
 
-      // start table for division
-      content += `
-        <table class="table">
-          <thead>
-            <tr>
-              <th colspan="7">
-      `;
+    divisionStandings.push(current);
+  } // end of each division loop
+
+  console.log(league + ' division standings:');
+  console.log(divisionStandings);
 
 
-      // each division title
-      for (let d = 0; d < divisions.length; d++) {
-        // MLB league response data has duplicate division names,
-        // therefore divCounter variable is used to distinguish which conference
-        if (league === 'mlb') {
-          let dCounter = 0;
-          if (d >= 3) {
-            dCounter = 1;
-          }
-          if ((divisionStandings[div][0].divisionRank.divisionName === divisions[d]) && (divisionStandings[div][0].conferenceRank.conferenceName === confs[dCounter])) {
-            content += `${divisions[d]} Division</th>`;
-          }
+  // main title html
+  content += `
+    <div class="title">
+      <h1>${league}: Regular Season Standings
+  `;
+
+  if (league === "nhl") {
+    content += nhlSeason;
+  } else if (league === "nba") {
+    content += nbaSeason;
+  } else if (league === "nfl") {
+    content += nflSeason;
+  } else {
+    content += mlbSeason;
+  }
+
+  content += `
+      </h1>
+    </div>
+  `;
+
+  // each conference html
+  for (let div = 0; div < divisions.length; div++) {
+    // MLB league response data has duplicate division names,
+    // therefore divCounter variable is used to distinguish which conference
+    if (league === 'mlb') {
+    let divCounter = 0;
+      if (div >= 3) {
+        divCounter = 1;
+      }
+
+      if ((divisions[div] === firstDivsInConfs[0]) && (divCounter === 0)) {
+        content += `
+          <div class="conf-title">
+            <h2>${confs[0]} Conference</h2>
+          </div>
+        `;
+      } else if ((divisions[div] === firstDivsInConfs[1]) && (divCounter === 1)) {
+          content += `
+            <div class="conf-title">
+              <h2>${confs[1]} Conference</h2>
+            </div>
+          `;
+      }
+    }
+    // all leagues other than MLB
+    else {
+      if (divisions[div] === firstDivsInConfs[0]) {
+        content += `
+          <div class="conf-title">
+            <h2>${confs[0]} Conference</h2>
+          </div>
+        `;
+      } else if (divisions[div] === firstDivsInConfs[1]) {
+          content += `
+            <div class="conf-title">
+              <h2>${confs[1]} Conference</h2>
+            </div>
+          `;
+      }
+    }
+
+
+    // start table for division
+    content += `
+      <table class="table">
+        <thead>
+          <tr>
+            <th colspan="7">
+    `;
+
+
+    // each division title
+    for (let d = 0; d < divisions.length; d++) {
+      // MLB league response data has duplicate division names,
+      // therefore divCounter variable is used to distinguish which conference
+      if (league === 'mlb') {
+        let dCounter = 0;
+        if (d >= 3) {
+          dCounter = 1;
         }
-        // all leagues other than MLB
-        else if (divisionStandings[div][0].divisionRank.divisionName === divisions[d]) {
+        if ((divisionStandings[div][0].divisionRank.divisionName === divisions[d]) && (divisionStandings[div][0].conferenceRank.conferenceName === confs[dCounter])) {
           content += `${divisions[d]} Division</th>`;
         }
       }
+      // all leagues other than MLB
+      else if (divisionStandings[div][0].divisionRank.divisionName === divisions[d]) {
+        content += `${divisions[d]} Division</th>`;
+      }
+    }
 
 
-      // table row headings
+    // table row headings
+    content += `
+      </tr>
+      <tr>
+      <td>Rank</td>
+      <td class="text-left">Team</td>
+      <td>GP</td>
+      <td>W</td>
+      <td>L</td>
+    `;
+
+    if ((league === "nba") || (league === "mlb")) {
       content += `
+        <td>Win%</td>
+        <td>GB</td>
+      `;
+    } else if (league === 'nfl') {
+      content += `
+        <td>T</td>
+        <td>Win%</td>
+      `;
+    } else {
+      content += `
+        <td>OTL</td>
+        <td>Pts</td>
+      `;
+    }
+
+    content += `
         </tr>
+      </thead>
+      <tbody>
+    `;
+
+
+    // each team data row for current division
+    for (let t = 0; t < divisionStandings[div].length; t++) {
+
+      let teamName = divisionStandings[div][t].team.name.toLowerCase();
+      teamName = teamName.replace(/\s/g, '');
+
+
+      content += `
         <tr>
-        <td>Rank</td>
-        <td class="text-left">Team</td>
-        <td>GP</td>
-        <td>W</td>
-        <td>L</td>
+        <td>${divisionStandings[div][t].divisionRank.rank}</td>
+        <td class="text-left">
       `;
 
-      if ((league === "nba") || (league === "mlb")) {
-        content += `
-          <td>Win%</td>
-          <td>GB</td>
-        `;
-      } else if (league === 'nfl') {
-        content += `
-          <td>T</td>
-          <td>Win%</td>
-        `;
-      } else {
-        content += `
-          <td>OTL</td>
-          <td>Pts</td>
-        `;
+      //  change team website url for Portland Trail Blazers (NBA)
+      if (teamName === 'trailblazers') {
+        content += `<a href="https://www.${league}.com/blazers" target="_blank">`;
+      }
+      //  change team website url for Arizona Diamondbacks (MLB)
+      else if (teamName === 'diamondbacks') {
+        content += `<a href="https://www.${league}.com/dbacks" target="_blank">`;
+      }
+      //  all other team website url's
+      else {
+        content += `<a href="https://www.${league}.com/${teamName}" target="_blank">`;
       }
 
       content += `
+            ${divisionStandings[div][t].team.city} ${divisionStandings[div][t].team.name}
+          </a>
+        </td>
+        <td>${divisionStandings[div][t].stats.gamesPlayed}</td>
+        <td>${divisionStandings[div][t].stats.standings.wins}</td>
+        <td>${divisionStandings[div][t].stats.standings.losses}</td>
+      `;
+
+
+      if ( (league === 'nba') || (league === 'mlb') ) {
+        content += `
+            <td>${divisionStandings[div][t].stats.standings.winPct}</td>
+            <td>${divisionStandings[div][t].overallRank.gamesBack}</td>
           </tr>
-        </thead>
-        <tbody>
-      `;
-
-
-      // each team data row for current division
-      for (let t = 0; t < divisionStandings[div].length; t++) {
-
-        let teamName = divisionStandings[div][t].team.name.toLowerCase();
-        teamName = teamName.replace(/\s/g, '');
-
-
-        content += `
-          <tr>
-          <td>${divisionStandings[div][t].divisionRank.rank}</td>
-          <td class="text-left">
         `;
-
-        //  change team website url for Portland Trail Blazers (NBA)
-        if (teamName === 'trailblazers') {
-          content += `<a href="https://www.${league}.com/blazers" target="_blank">`;
-        }
-        //  change team website url for Arizona Diamondbacks (MLB)
-        else if (teamName === 'diamondbacks') {
-          content += `<a href="https://www.${league}.com/dbacks" target="_blank">`;
-        }
-        //  all other team website url's
-        else {
-          content += `<a href="https://www.${league}.com/${teamName}" target="_blank">`;
-        }
-
+      } else if (league === 'nfl') {
         content += `
-              ${divisionStandings[div][t].team.city} ${divisionStandings[div][t].team.name}
-            </a>
-          </td>
-          <td>${divisionStandings[div][t].stats.gamesPlayed}</td>
-          <td>${divisionStandings[div][t].stats.standings.wins}</td>
-          <td>${divisionStandings[div][t].stats.standings.losses}</td>
+            <td>${divisionStandings[div][t].stats.standings.ties}</td>
+            <td>${divisionStandings[div][t].stats.standings.winPct}</td>
+          </tr>
         `;
+      } else {
+        content += `
+            <td>${divisionStandings[div][t].stats.standings.overtimeLosses}</td>
+            <td>${divisionStandings[div][t].stats.standings.points}</td>
+          </tr>
+        `;
+      }
+
+    } // end division -for- loop
 
 
-        if ( (league === 'nba') || (league === 'mlb') ) {
-          content += `
-              <td>${divisionStandings[div][t].stats.standings.winPct}</td>
-              <td>${divisionStandings[div][t].overallRank.gamesBack}</td>
-            </tr>
-          `;
-        } else if (league === 'nfl') {
-          content += `
-              <td>${divisionStandings[div][t].stats.standings.ties}</td>
-              <td>${divisionStandings[div][t].stats.standings.winPct}</td>
-            </tr>
-          `;
-        } else {
-          content += `
-              <td>${divisionStandings[div][t].stats.standings.overtimeLosses}</td>
-              <td>${divisionStandings[div][t].stats.standings.points}</td>
-            </tr>
-          `;
-        }
-
-      } // end division -for- loop
+    // end of table after all divisions are complete
+    content += `
+        </tbody>
+      </table>
+    `;
+  } // end conference -for- loop
 
 
-      // end of table after all divisions are complete
-      content += `
-          </tbody>
-        </table>
-      `;
-    } // end conference -for- loop
+  // access html in global scope variable, standingsContent
+  if (league === 'nhl') {
+    nhlContent = content;
+  } else if (league === 'nba') {
+    nbaContent = content;
+  } else if (league === 'nfl') {
+    nflContent = content;
+  } else {
+    mlbContent = content;
+  }
+
+} // end getStandings function
 
 
-    // access html in global scope variable, standingsContent
-    if (league === 'nhl') {
-      nhlContent = content;
-    } else if (league === 'nba') {
-      nbaContent = content;
-    } else if (league === 'nfl') {
-      nflContent = content;
-    } else {
-      mlbContent = content;
-    }
-
-  } // end getStandings function
-
-
-//======================================
-//======================================
+/*=====================================================
+  Receive data functions from index.js
+  and execute getStandings function for each league
+=====================================================*/
 
 // Receive nhl data from index.js
 socket.on('nhl-data', function(data) {
@@ -345,6 +347,8 @@ socket.on('nhl-data', function(data) {
   // call getStandings function, passings nhl data & divisions
   getStandings(data, totalTeams, confs, firstDivsInConfs, divisions, divisionStandings, overallStandings, league);
   
+  count++;
+  isReady();
 });
 
 
@@ -366,9 +370,11 @@ socket.on('nba-data', function(data) {
   const overallStandings = [];
   const divisionStandings = [];
 
-  // call getStandings function, passings nhl data & divisions
+  // call getStandings function, passings nba data & divisions
   getStandings(data, totalTeams, confs, firstDivsInConfs, divisions, divisionStandings, overallStandings, league);
   
+  count++;
+  isReady();
 });
 
 
@@ -390,9 +396,11 @@ socket.on('nfl-data', function(data) {
   const overallStandings = [];
   const divisionStandings = [];
 
-  // call getStandings function, passings nhl data & divisions
+  // call getStandings function, passings nfl data & divisions
   getStandings(data, totalTeams, confs, firstDivsInConfs, divisions, divisionStandings, overallStandings, league);
   
+  count++;
+  isReady();
 });
 
 
@@ -404,7 +412,6 @@ socket.on('mlb-data', function(data) {
   console.log('this is the mlb data:');
   console.log(mlbData);
 
-  //let count = 0;
   // set mlb variables for getStandings function arguments
   const league = 'mlb';
   const confs = ['American League', 'National League'];
@@ -414,7 +421,9 @@ socket.on('mlb-data', function(data) {
   const overallStandings = [];
   const divisionStandings = [];
 
-  // call getStandings function, passings nhl data & divisions
+  // call getStandings function, passings mlb data & divisions
   getStandings(data, totalTeams, confs, firstDivsInConfs, divisions, divisionStandings, overallStandings, league);
   
+  count++;
+  isReady();
 });
