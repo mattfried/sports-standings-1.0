6841007abc1c63b1b1ba84f57f46f306fa91416b
Matt Fried <mattfrieder@gmail.com>
2019-05-28 21:39:48 -0500

    index.html moved to public dir, index.js serves all public static files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //==========express=============
 const app = require('express')();
+const express = require('express');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const data = require('./data.js');
@@ -14,10 +15,19 @@ data.getData().then((result) => {
   latestData = result;
 });
 
-
+// Route created at the root of the site "/"
+// The result of the route returns HTML file: index.html
+/*
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+*/
+
+/***** The following code for serving static files
+       supersedes the route code above *****/
+
+// Serve all static files in 'public' directory
+app.use(express.static('public'));
 
 http.listen(3000, function() {
   console.log('HTTP server started on port 3000');
