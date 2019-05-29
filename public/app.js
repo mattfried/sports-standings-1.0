b06598adc36ad57ee2aa9877d680b49f1ad41ae9
Matt Fried <mfried@WOLFROMENG.local>
2019-05-29 10:40:06 -0500

    updated index.html with buttons and standings div

--- public/app.js
+++ public/app.js
@@ -1,9 +1,12 @@
 const socket = io();
 
+// Receive nhl data from index.js
 socket.on('nhl-data', function(data) {
   let nhlData;
   nhlData = data;
   
+  console.log('this is the data:');
   console.log(nhlData);
-  console.log('do stuff here with sports data');
+
+
 });
